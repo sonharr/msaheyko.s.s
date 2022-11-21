088a1c28f8ccc482393f2370e9d19c64bf01e239
sonharr <116548514+sonharr@users.noreply.github.com>
2022-11-21 10:07:04 +0300

    Add files via upload

---
+++ Z3.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Xml;
+
+namespace HelloApp
+{
+    class User
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public string Company { get; set; }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<User> users = new List<User>();
+            XmlDocument xDoc = new XmlDocument();
+            xDoc.Load(@"D:\Документы\users.xml");
+            XmlElement xRoot = xDoc.DocumentElement;
+
+            XmlElement userElem = xDoc.CreateElement("user");  
+            XmlAttribute nameAttr = xDoc.CreateAttribute("name");  
+            XmlElement companyElem = xDoc.CreateElement("company");  
+            XmlElement ageElem = xDoc.CreateElement("age");  
+
+            Console.WriteLine("Введите имя:");
+            string file1 = Console.ReadLine();
+            XmlText nameText = xDoc.CreateTextNode(file1);
+
+            Console.WriteLine();
+            Console.WriteLine("Введите компанию:");
+            string file2 = Console.ReadLine();
+            XmlText companyText = xDoc.CreateTextNode(file2);
+
+            Console.WriteLine();
+            Console.WriteLine("Введите возраст:");
+            string file3 = Console.ReadLine();
+            XmlText ageText = xDoc.CreateTextNode(file3);
+
+            nameAttr.AppendChild(nameText);
+            companyElem.AppendChild(companyText);
+            ageElem.AppendChild(ageText);
+            userElem.Attributes.Append(nameAttr);
+            userElem.AppendChild(companyElem);
+            userElem.AppendChild(ageElem);
+            xRoot.AppendChild(userElem);
+            xDoc.Save(@"D:\Документы\users.xml");
+            Console.WriteLine();
+            foreach (XmlElement xnode in xRoot)
+            {
+                User user = new User();
+                XmlNode attr = xnode.Attributes.GetNamedItem("name");
+                if (attr != null)
+                    user.Name = attr.Value;
+
+                foreach (XmlNode childnode in xnode.ChildNodes)
+                {
+                    if (childnode.Name == "company")
+                        user.Company = childnode.InnerText;
+
+                    if (childnode.Name == "age")
+                        user.Age = Int32.Parse(childnode.InnerText);
+                }
+                users.Add(user);
+            }
+            foreach (User u in users)
+                Console.WriteLine($"{u.Name} ({u.Company}) - {u.Age}");
+
+            string path1 = @"D:\Документы\users.xml";
+            FileInfo fileInf = new FileInfo(path1);
+            Console.WriteLine("После нажатия ENTER файл будет удалён:");
+            string enter = Console.ReadLine();
+            if (enter == "")
+                if (fileInf.Exists)
+                {
+                    fileInf.Delete();
+                }
+        }
+    }
+}
---
+++ Z5.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.IO;
+using System.IO.Compression;
+
+namespace HelloApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string dirName = @"D:\";
+            if (Directory.Exists(dirName))
+            {
+                Console.WriteLine("Подкаталоги D:");
+                string[] dirs = Directory.GetDirectories(dirName);
+                foreach (string s in dirs)
+                {
+                    Console.WriteLine(s);
+                }
+            }
+            string path = @"D:\";
+            DirectoryInfo dirInfo = new DirectoryInfo(path);
+            if (!dirInfo.Exists)
+            {
+                dirInfo.Create();
+            }
+            Console.WriteLine();
+            Console.WriteLine("Выберете папку: ");
+            string papka = Console.ReadLine();
+            string sourceFolder = papka; // исходнаяпапка
+            string zipFile = @"D:\test.zip"; // сжатыйфайл
+            string targetFolder = @"D:\newtest"; // папка, куда распаковывается файл
+
+            Console.WriteLine();
+            ZipFile.CreateFromDirectory(sourceFolder, zipFile);
+            Console.WriteLine($"Выбранная папка, {sourceFolder}, заархивирована в файл{zipFile}");
+
+            Console.WriteLine();
+            FileInfo fileInf = new FileInfo(zipFile);
+            if (fileInf.Exists)
+            {
+                Console.WriteLine("Имя файла: {0}", fileInf.Name);
+                Console.WriteLine("Время создания: {0}", fileInf.CreationTime);
+                Console.WriteLine("Размер: {0}", fileInf.Length);
+            }
+
+            ZipFile.ExtractToDirectory(zipFile, targetFolder);
+            Console.WriteLine($"Файл {zipFile} распакован в папку{targetFolder}");
+
+            Console.WriteLine();
+            Console.WriteLine("Файлы из распакованной папки: ");
+            string[] files3 = Directory.GetFiles(targetFolder);
+            foreach (string s in files3)
+            {
+                Console.WriteLine(s);
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("Выберете файл, о котором хотите посмотреть информацию:");
+            string file3 = Console.ReadLine();
+
+            Console.WriteLine();
+            FileInfo fileInf1 = new FileInfo(file3);
+            if (fileInf.Exists)
+            {
+                Console.WriteLine("Имя файла: {0}", fileInf1.Name);
+                Console.WriteLine("Время создания: {0}", fileInf1.CreationTime);
+                Console.WriteLine("Размер: {0}", fileInf1.Length);
+            }
+
+            Console.WriteLine();
+            string path3 = $"{path}test.zip";
+            FileInfo fileInf3 = new FileInfo(path3);
+            Console.WriteLine($"После нажатия ENTER файл {path}test.zip будет удалён:");
+            string enter = Console.ReadLine();
+            if (enter == "")
+            {
+                if (fileInf3.Exists)
+                {
+                    fileInf3.Delete();
+                }
+            }
+            Console.WriteLine();
+            Console.WriteLine($"После нажатия ENTER файл {path} newtest будет удалён:");
+            string enter1 = Console.ReadLine();
+            if (enter1 == "")
+            {
+                string path4 = @"D:\newtest";
+                Directory.Delete(path4, true);
+            }
+        }
+    }
+}
